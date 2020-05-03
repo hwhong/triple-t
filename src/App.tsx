4873fa7888675236c93059c90f56a325f0703ab3
warren <hwhong1129@gmail.com>
2020-05-02 18:23:26 -0700

    hicup

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,52 @@ import React, { useState } from "react";
 import styles from "./main.module.css";
 import { GridCell } from "./components/grid-cell";
 
+const valueMap: { [type: string]: number } = { O: 1, X: -1 };
+
 function App() {
   const [cellArray, setCellArray] = useState(Array(9).fill(" "));
   const [currentPlayer, setCurrentPlayer] = useState("O");
+  const [row, setRow] = useState<number[]>([0, 0, 0]);
+  const [col, setCol] = useState([0, 0, 0]);
+  const [diag, setDiag] = useState(0);
+  const [xdiag, setXdiag] = useState(0);
   const handleClick = (i: number) => {
-    cellArray[i] = currentPlayer;
+    if (cellArray[i] === " ") {
+      cellArray[i] = currentPlayer;
+      setCellArray([...cellArray]);
+      setCurrentPlayer(currentPlayer === "O" ? "X" : "O");
+      checkWinner(i);
+    }
+  };
 
-    setCellArray([...cellArray]);
-    setCurrentPlayer(currentPlayer === "O" ? "X" : "O");
+  const checkWinner = (i: number) => {
+    const r = i % 3;
+    const c = Math.round(i / 3);
+
+    row[r] += valueMap[currentPlayer];
+    setRow([...row]);
+    col[c] += valueMap[currentPlayer];
+    setCol([...col]);
+    if (r === c) {
+      setDiag(diag + valueMap[currentPlayer]);
+    }
+
+    // X-diagonal
+    if (r + c === 2) {
+      setXdiag(xdiag + valueMap[currentPlayer]);
+    }
+    // console.log(row);
+    // console.log(col);
+    // console.log(diag);
+    // console.log(xdiag);
+    if (
+      Math.abs(row[r]) === 3 ||
+      Math.abs(col[c]) === 3 ||
+      Math.abs(diag) === 3 ||
+      Math.abs(xdiag) === 3
+    ) {
+      alert(`${currentPlayer} WON!`);
+    }
   };
 
   return (
